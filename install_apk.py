f5a10ce75f482ffbe4af65bba175e0f0c20add96
wankai <wk99887766@gmail.com>
2021-04-07 15:26:58 +0800

    更新install工具

--- install_apk.py
+++ install_apk.py
@@ -19,6 +19,7 @@ projects_details = [
     ["NoteAS", "C:\\Work\\ASProjects\\", "\\Note", "com.task.notes"],
     ["WeatherAccurate", "C:\\Work\\ASProjects\\", "\\WeatherForecast", "accurate.local.weather.forecast.channel"],
     ["WeatherForecastAS", "C:\\Work\\ASProjects\\", "\\WeatherForecast", "weather.forecast.live.weather"],
+    ["WeatherForecastBM", "C:\\Work\\ASProjects\\", "\\WeatherForecast", "weather.local.weather.forecast.channel"],
     ["LauncherIOS", "C:\\Work\\ASProjects\\", "\\app", "theme.wallpaper.smart.launcher"],
     ["AdvertLibraryExampleAS", "C:\\Work\\ASProjects\\", "\\app", "com.ijoysoft.advert"],
 ]
